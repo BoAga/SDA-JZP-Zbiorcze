e9006ace0b26badd42e14b40bb016c7696e24ab7
Aga <aga@git.pl>
2020-08-01 12:32:24 +0200

    Solved ex6

---
+++ src/main/java/pl/sda/ex6/TreeMapService.java
@@ -0,0 +1,15 @@
+package pl.sda.ex6;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class TreeMapService {
+
+    public void printFirstAndLastEntry (TreeMap col){
+        
+
+
+
+
+    }
+}
