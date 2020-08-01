537441edd166b1b8e30107272abfd3ff2267d9ec
Aga <aga@git.pl>
2020-08-01 15:14:49 +0200

    Solved ex10

--- src/test/java/pl/sda/ex9/CircleTest.java
+++ src/test/java/pl/sda/ex9/CircleTest.java
@@ -1,15 +1,16 @@
 package pl.sda.ex9;
 
 import org.junit.jupiter.api.Test;
+import pl.sda.ex10.MoveDirection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CircleTest {
 
     @Test
-    public void testRightCharacteristic(){
+    public void testRightCharacteristicAfterMove(){
         Point2D centre = new Point2D(0, 0);
-        Point2D point = new Point2D(1, 0);
+        Point2D point = new Point2D(0, 1);
 
         Circle circle = new Circle(centre, point);
         double r = circle.getRadius();
@@ -18,5 +19,12 @@ public class CircleTest {
         assertEquals(2 * Math.PI * r, circle.getParameter());
         assertEquals(Math.PI * r * r, circle.getArea());
 
+        MoveDirection md = new MoveDirection(2.5d, -3.0);
+        circle.move(md);
+        assertEquals(2.5 , circle.getCentre().getX());
+        assertEquals(-2 , circle.getPoint().getY());
+
     }
+
+
 }
