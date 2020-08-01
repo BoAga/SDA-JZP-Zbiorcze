c1f97f9a878c9282cf291f559bcef9942d0e7141
Aga <aga@git.pl>
2020-08-01 13:14:25 +0200

    Solved ex7

--- src/main/java/pl/sda/ex6/TreeMapService.java
+++ src/main/java/pl/sda/ex6/TreeMapService.java
@@ -6,7 +6,8 @@ import java.util.TreeSet;
 public class TreeMapService {
 
     public void printFirstAndLastEntry (TreeMap col){
-        
+        System.out.println(col.firstEntry());
+        System.out.println(col.lastEntry());
 
 
 
